21095a43bf6ac4c8f2c5bf5a2d64a4bd4cca9654
Vikash Kumar <96245971+Vikash0122@users.noreply.github.com>
2023-01-16 23:14:42 +0530

    Create A+B?.cpp

---
+++ Difficulty=800/A+B?.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        char a[5];
+        cin>>a;
+        cout<<(a[0]+a[2]-96)<<endl;
+    }
+    return 0;
+}
