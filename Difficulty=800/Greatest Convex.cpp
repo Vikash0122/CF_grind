f1d04a0a64e63141043249ab88888863055b9525
Vikash Kumar <96245971+Vikash0122@users.noreply.github.com>
2023-02-10 23:33:07 +0530

    Create Greatest Convex.cpp

---
+++ Difficulty=800/Greatest Convex.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+int t;
+cin>>t;
+while(t--)
+{
+long int k;
+cin>>k;
+cout<<k-1<<"\n";
+}
+return 0;
+}//vikash0122
