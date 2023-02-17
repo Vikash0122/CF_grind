58c13ff79fadbb300ca4085534b29070f1637a52
Vikash Kumar <96245971+Vikash0122@users.noreply.github.com>
2023-02-17 23:56:32 +0530

    Create Game with sticks.cpp

---
+++ Difficulty=900/Game with sticks.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    n = min(n, m);
+    if(n&1) cout<<"Akshat"<<endl;
+    else cout<<"Malvika"<<endl;
+    return 0;
+}
+//vikash0122
