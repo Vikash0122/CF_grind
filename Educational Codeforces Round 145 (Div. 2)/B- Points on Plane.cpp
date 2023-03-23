f4b71d1f543f2b0af98038f0b032f4c87346249e
Vikash Kumar <96245971+Vikash0122@users.noreply.github.com>
2023-03-23 21:47:03 +0530

    Create B- Points on Plane.cpp

---
+++ Educational Codeforces Round 145 (Div. 2)/B- Points on Plane.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long int
+#define endl "\n"
+#define PI 3.141592653589793238462
+#define mod 1000000007
+
+void solve(){
+ll n;
+cin>>n;
+ll ans = sqrtl(n-1);
+cout<<ans<<endl;
+}
+
+int main(){
+FIN;
+ll t;
+cin>>t;
+while(t--){
+solve();
+}
+return 0;
+}
