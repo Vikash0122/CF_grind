580865d7b1ecd0f083c4bea3d58ca4bd595c78b6
Vikash Kumar <96245971+Vikash0122@users.noreply.github.com>
2023-04-09 23:27:24 +0530

    Create A- Ian Visits Mary.cpp

---
+++ Codeforces Round 865 (Div. 2)/A- Ian Visits Mary.cpp
@@ -0,0 +1,27 @@
+ //Codeforces Round 865 (Div. 2)
+#include<bits/stdc++.h>
+using namespace std;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long int
+#define endl "\n"
+#define PI 3.141592653589793238462
+#define mod 1000000007
+
+void solve(){
+ll a,b;
+cin>>a>>b;
+
+cout<<2<<endl;
+cout<<a-1<<" "<<1<<endl;
+cout<<a<<" "<<b<<endl;
+}
+
+int main(){
+FIN;
+ll t;
+cin>>t;
+while(t--){
+solve();
+}
+return 0;
+}
